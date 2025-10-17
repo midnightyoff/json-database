287b93ba0bbe3acf731673b60d877dee0582c873
Aleksandr Yurk <yurk.9999@mail.ru>
2025-10-17 18:29:29 +0700

    Initial commit. Create dir`s for client and server.

---
+++ src/main/java/com/projects/client/Main.java
@@ -0,0 +1,7 @@
+package com.projects.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is client");
+    }
+}
---
+++ src/main/java/com/projects/server/Main.java
@@ -0,0 +1,7 @@
+package com.projects.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is server");
+    }
+}
